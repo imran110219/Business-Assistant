fbf229cc28e75259403c7ecb0334b3e92b812e18
Sadman Sobhan <imran110219@gmail.com>
2021-11-08 23:35:28 +0600

    Starter Template

---
+++ src/test/java/com/moulik/businessassistant/BusinessassistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moulik.businessassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessassistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
