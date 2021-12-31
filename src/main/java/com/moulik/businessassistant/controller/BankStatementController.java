33e7212d375046c23a00d3f9634e783bd93e1955
Sadman Sobhan <imran110219@gmail.com>
2022-01-01 02:10:03 +0600

    add datatable

--- src/main/java/com/moulik/businessassistant/controller/BankStatementController.java
+++ src/main/java/com/moulik/businessassistant/controller/BankStatementController.java
@@ -1,18 +1,20 @@
 package com.moulik.businessassistant.controller;
 
 import com.moulik.businessassistant.dto.BankStatementDTO;
+import com.moulik.businessassistant.dto.ProductDTO;
+import com.moulik.businessassistant.exception.RecordNotFoundException;
 import com.moulik.businessassistant.iservice.BankStatementService;
 import com.moulik.businessassistant.iservice.UserService;
 import com.moulik.businessassistant.model.BankStatement;
 import com.moulik.businessassistant.model.BankStatement;
+import com.moulik.businessassistant.model.Customer;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -38,5 +40,25 @@ public class BankStatementController {
     public ResponseEntity<String> addBankStatement(BankStatementDTO bankStatementDTO){
         bankStatementService.createBankStatement(modelMapper.map(bankStatementDTO, BankStatement.class));
         return new ResponseEntity<>("BankStatement is added successfully", HttpStatus.OK);
+    }
+
+    @GetMapping("/bankstatements/{id}")
+    public ResponseEntity<BankStatementDTO> getBankStatementById(@PathVariable(value = "id") Long bankStatementId)
+            throws RecordNotFoundException {
+        BankStatementDTO bankStatementDTO = modelMapper.map(bankStatementService.getBankStatementById(bankStatementId), BankStatementDTO.class);
+        return ResponseEntity.ok(bankStatementDTO);
+    }
+
+    @PutMapping("/bankstatements/edit/{id}")
+    public ResponseEntity<String> editBankStatementById(@RequestBody BankStatementDTO newBankStatementDTO, @PathVariable(value = "id") Long bankStatementId) {
+        bankStatementService.updateBankStatement(modelMapper.map(newBankStatementDTO, BankStatement.class), bankStatementId);
+        return new ResponseEntity<>("BankStatement is Edited Successfully", HttpStatus.OK);
+    }
+
+    @DeleteMapping("/bankstatements/delete/{id}")
+    @ResponseBody
+    public ResponseEntity<String> deleteBankStatementById(@PathVariable(value = "id") Long bankStatementId){
+        bankStatementService.deleteBankStatementById(bankStatementId);
+        return new ResponseEntity<>("BankStatement is Deleted Successfully", HttpStatus.OK);
     }
 }
